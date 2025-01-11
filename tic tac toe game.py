06d45ecf9656a3832f8c4ee7a5040edbf98f85d3
KalpKanta <Exoplayz72@gmail.com>
2025-01-11 16:41:02 +0000

    Add files via upload

--- tic tac toe game.py
+++ tic tac toe game.py
@@ -4,9 +4,8 @@ import random
 
 root = tk.Tk()
 root.title("tic tac toe game")
-root.geometry("800x600")
+root.geometry("230x260")
 
-nbv = ["", "", "", "", "", "", "", "" ,""]
 
 if random.randint(1,2) == 1:
     playerS = "X"
@@ -20,18 +19,24 @@ def XO2(button, index):
     if button in ap:
         button.config(text = playerS)
         ap.remove(button)
-    button = random.choice(ap)
-    button.config(text = computerS)
-    ap.remove(button)
+        win()
+        button = random.choice(ap)
+        button.config(text = computerS)
+        ap.remove(button)
+        win()
 
 def win():
     winc = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 4, 8), (2, 4, 6),(0, 3, 6), (1, 4, 7), (2, 5, 8)]
-    for i in range(9):
-        if nbv[winc[i](0)] == playerS and nbv[winc[i](1)] == playerS and nbv[winc[i](2)] == playerS:
+    for i in range(8):
+        if nbv[winc[i][0]]["text"] == playerS and nbv[winc[i][1]]["text"] == playerS and nbv[winc[i][2]]["text"] == playerS:
             tkinter.messagebox.showinfo("Player Won!", "You have won the game!")
-        if nbv[winc[i](0)] == computerS and nbv[winc[i](1)] == computerS and nbv[winc[i](2)] == computerS:
+            break
+        elif nbv[winc[i][0]]["text"] == computerS and nbv[winc[i][1]]["text"] == computerS and nbv[winc[i][2]]["text"] == computerS:
             tkinter.messagebox.showinfo("Computer Won!", "You have lost the game!")
-
+            break
+        elif len(ap) == 0:
+            tkinter.messagebox.showinfo("Tie game!", "The game is a tie!")
+            break
 player_label = tk.Label(root, text = "Player = " + playerS)
 player_label.grid(row = 1, column = 1)
 computer_label = tk.Label(root, text = "Computer = " + computerS)
@@ -57,5 +62,6 @@ grid9 = tk.Button(root, width = 9, height =4, command = lambda:XO2(grid9, 8))
 grid9.grid(row = 5, column = 3)
 
 ap = [grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9]
+nbv = [grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9]
 
 root.mainloop()
